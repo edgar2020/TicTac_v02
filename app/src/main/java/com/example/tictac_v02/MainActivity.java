b9e4f2df09c2fb6f7a7aefaf31b3ea4d4d1ad255
VictorDominguez5348 <54280418+VictorDominguez5348@users.noreply.github.com>
2019-09-24 08:15:04 -0700

    Functionality of Button

--- app/src/main/java/com/example/tictac_v02/MainActivity.java
+++ app/src/main/java/com/example/tictac_v02/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.tictac_v02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button start_button = (Button) findViewById(R.id.startButton);
+        start_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), gameScreen.class);
+                startActivityForResult(myIntent, 0);
+            }
+
+        });
     }
 }
