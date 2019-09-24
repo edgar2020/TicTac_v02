f2055d821c7ee2fb8676c94564e569179036e50d
Edgar Melendrez <evmelendrez@gmail.com>
2019-09-24 08:20:49 -0700

    Update gameScreen.java

--- app/src/main/java/com/example/tictac_v02/gameScreen.java
+++ app/src/main/java/com/example/tictac_v02/gameScreen.java
@@ -2,7 +2,11 @@ package com.example.tictac_v02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class gameScreen extends AppCompatActivity {
 
@@ -10,5 +14,30 @@ public class gameScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gamescreen);
+
+        final boolean player1Turn=true;
+
+        Button button1 = (Button) findViewById(R.id.button);
+        Button button2 = (Button) findViewById(R.id.button2);
+        Button button3 = (Button) findViewById(R.id.button3);//first row
+
+        Button button4 = (Button) findViewById(R.id.button4);
+        Button button5 = (Button) findViewById(R.id.button5);
+        Button button6 = (Button) findViewById(R.id.button6);//second row
+
+        Button button7 = (Button) findViewById(R.id.button7);
+        Button button8 = (Button) findViewById(R.id.button8);
+        Button button9 = (Button) findViewById(R.id.button9);//third row
+
+        Button ticTacButtons[] = {button1, button2, button3,button4,button5,button6,button7,button8,button9};
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(player1Turn)
+                    ((Button) findViewById(R.id.button)).setText("X");
+                else
+                    ((Button) findViewById(R.id.button)).setText("O");
+            }
+        });
     }
 }
