ddadca1a84bd10b02564b79e22bf0908a93a74da
edgar2020 <evmelendrez@gmail.com>
2019-09-30 08:46:41 -0700

    first attempt to add changing button

--- app/src/main/java/com/example/tictac_v02/ticTacScreen.java
+++ app/src/main/java/com/example/tictac_v02/ticTacScreen.java
@@ -14,7 +14,12 @@ public class ticTacScreen extends AppCompatActivity {
     boolean gameover=false;
 
     Button button1, button2, button3,button4,button5,button6,button7,button8,button9;
+    TextView playersTurn;
 
+
+
+    int player1Score=0;
+    int player2Score=0;
 
     int turns=0;
     @Override
@@ -36,7 +41,8 @@ public class ticTacScreen extends AppCompatActivity {
 
 
 
-        TextView playersTurn = (TextView)findViewById(R.id.playerTurn);
+        playersTurn = (TextView)findViewById(R.id.playerTurn);
+
 
 
             button1.setOnClickListener(new View.OnClickListener() {
@@ -173,69 +179,156 @@ public class ticTacScreen extends AppCompatActivity {
 
     public void checkWin()
     {
+        if(player1Turn)
+            playersTurn.setText("Player 1's Turn");
+       else
+            playersTurn.setText("Player 2's Turn");
         Button ticTacButtons[] = {button1, button2, button3,button4,button5,button6,button7,button8,button9};
         if((ticTacButtons[0].getText()==ticTacButtons[1].getText())&&(ticTacButtons[0].getText()==ticTacButtons[2].getText()))
         {
             if(ticTacButtons[0].getText()=="X")
-                Toast.makeText(this, "Player 1", Toast.LENGTH_LONG).show();
+            {
+                Toast.makeText(this, "Player 1 Wins", Toast.LENGTH_LONG).show();
+                player1Score++;
+                restartGame();
+            }
             if(ticTacButtons[0].getText()=="O")
-                Toast.makeText(this, "Player 2", Toast.LENGTH_LONG).show();
+            {
+                Toast.makeText(this, "Player 2 WIns", Toast.LENGTH_LONG).show();
+                player2Score++;
+                restartGame();
+            }
         }
 
         if((ticTacButtons[0].getText()==ticTacButtons[4].getText())&&(ticTacButtons[0].getText()==ticTacButtons[8].getText()))
         {
             if(ticTacButtons[0].getText()=="X")
-                Toast.makeText(this, "Player 1", Toast.LENGTH_LONG).show();
+            {
+                Toast.makeText(this, "Player 1 Wins", Toast.LENGTH_LONG).show();
+                player1Score++;
+                restartGame();
+            }
             if(ticTacButtons[0].getText()=="O")
-                Toast.makeText(this, "Player 2", Toast.LENGTH_LONG).show();
+            {
+                Toast.makeText(this, "Player 2 WIns", Toast.LENGTH_LONG).show();
+                player2Score++;
+                restartGame();
+            }
         }
 
         if((ticTacButtons[0].getText()==ticTacButtons[3].getText())&&(ticTacButtons[0].getText()==ticTacButtons[6].getText()))
         {
             if(ticTacButtons[0].getText()=="X")
-                Toast.makeText(this, "Player 1", Toast.LENGTH_LONG).show();
+            {
+                Toast.makeText(this, "Player 1 Wins", Toast.LENGTH_LONG).show();
+                player1Score++;
+                restartGame();
+            }
             if(ticTacButtons[0].getText()=="O")
-                Toast.makeText(this, "Player 2", Toast.LENGTH_LONG).show();
+            {
+                Toast.makeText(this, "Player 2 WIns", Toast.LENGTH_LONG).show();
+                player2Score++;
+                restartGame();
+            }
         }
 
         if((ticTacButtons[3].getText()==ticTacButtons[4].getText())&&(ticTacButtons[3].getText()==ticTacButtons[5].getText()))
         {
             if(ticTacButtons[3].getText()=="X")
-                Toast.makeText(this, "Player 1", Toast.LENGTH_LONG).show();
+            {
+                Toast.makeText(this, "Player 1 Wins", Toast.LENGTH_LONG).show();
+                player1Score++;
+                restartGame();
+            }
             if(ticTacButtons[3].getText()=="O")
-                Toast.makeText(this, "Player 2", Toast.LENGTH_LONG).show();
+            {
+                Toast.makeText(this, "Player 2 WIns", Toast.LENGTH_LONG).show();
+                player2Score++;
+                restartGame();
+            }
         }
 
-        if((ticTacButtons[6].getText()==ticTacButtons[7].getText())&&(ticTacButtons[6].getText()==ticTacButtons[5].getText()))
+        if((ticTacButtons[6].getText()==ticTacButtons[7].getText())&&(ticTacButtons[6].getText()==ticTacButtons[8].getText()))
         {
             if(ticTacButtons[6].getText()=="X")
-                Toast.makeText(this, "Player 1", Toast.LENGTH_LONG).show();
+            {
+                Toast.makeText(this, "Player 1 Wins", Toast.LENGTH_LONG).show();
+                player1Score++;
+                restartGame();
+            }
             if(ticTacButtons[6].getText()=="O")
-                Toast.makeText(this, "Player 2", Toast.LENGTH_LONG).show();
+            {
+                Toast.makeText(this, "Player 2 WIns", Toast.LENGTH_LONG).show();
+                player2Score++;
+                restartGame();
+
+            }
         }
 
         if((ticTacButtons[2].getText()==ticTacButtons[5].getText())&&(ticTacButtons[2].getText()==ticTacButtons[8].getText()))
         {
             if(ticTacButtons[2].getText()=="X")
-                Toast.makeText(this, "Player 1", Toast.LENGTH_LONG).show();
+            {
+                Toast.makeText(this, "Player 1 Wins", Toast.LENGTH_LONG).show();
+                player1Score++;
+                restartGame();
+            }
             if(ticTacButtons[2].getText()=="O")
-                Toast.makeText(this, "Player 2", Toast.LENGTH_LONG).show();
+            {
+                Toast.makeText(this, "Player 2 WIns", Toast.LENGTH_LONG).show();
+                player2Score++;
+                restartGame();
+            }
         }
 
         if((ticTacButtons[1].getText()==ticTacButtons[4].getText())&&(ticTacButtons[1].getText()==ticTacButtons[7].getText()))
         {
             if(ticTacButtons[1].getText()=="X")
-                Toast.makeText(this, "Player 1", Toast.LENGTH_LONG).show();
+            {
+                Toast.makeText(this, "Player 1 Wins", Toast.LENGTH_LONG).show();
+                player1Score++;
+                restartGame();
+            }
             if(ticTacButtons[1].getText()=="O")
-                Toast.makeText(this, "Player 2", Toast.LENGTH_LONG).show();
+            {
+                Toast.makeText(this, "Player 2 WIns", Toast.LENGTH_LONG).show();
+                player2Score++;
+                restartGame();
+            }
         }
 
         if((ticTacButtons[2].getText()==ticTacButtons[4].getText())&&(ticTacButtons[2].getText()==ticTacButtons[6].getText()))
         {
             if(ticTacButtons[2].getText()=="X")
-                Toast.makeText(this, "Player 1", Toast.LENGTH_LONG).show();
+            {
+                Toast.makeText(this, "Player 1 Wins", Toast.LENGTH_LONG).show();
+                player1Score++;
+                restartGame();
+            }
             if(ticTacButtons[2].getText()=="O")
-                Toast.makeText(this, "Player 2", Toast.LENGTH_LONG).show();
+            {
+                Toast.makeText(this, "Player 2 WIns", Toast.LENGTH_LONG).show();
+                player2Score++;
+                restartGame();
+            }
         }
+    }
+    public void restartGame()
+    {
+        button1.setText("");
+        button2.setText("");
+        button3.setText("");
+        button4.setText("");
+        button5.setText("");
+        button6.setText("");
+        button7.setText("");
+        button8.setText("");
+        button9.setText("");
+
+        TextView P1Score = (TextView)findViewById(R.id.P1Score);
+        TextView P2Score = (TextView)findViewById(R.id.P2Score);
+
+        P1Score.setText("Player 1 Score: "+player1Score);
+        P2Score.setText("Player 2 Score: "+player2Score);
     }
 }
